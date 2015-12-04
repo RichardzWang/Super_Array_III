56ebd3fb414a0670eb07935433eecb6c9a7a2f41
James Cao <jcao14@stuy.edu>
2015-12-04 09:29:10 -0500

    Added interface

---
+++ ListInt.java
@@ -0,0 +1,10 @@
+public interface ListInt{
+
+    void add (int item);
+    void add (int item, int index);
+    void remove (int index);
+    int size ();
+    int get(int index);
+    int set (int item, int index);
+
+}
